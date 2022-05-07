315d0dabd18a2fcb4d43524a437bc89dc2cef1b5
Evaldas Grublys <evaldasg@wix.com>
2022-05-07 13:38:18 +0300

    Remove unnecessary comment

--- EKrumynas/AutoMapperProfile.cs
+++ EKrumynas/AutoMapperProfile.cs
@@ -2,24 +2,6 @@
 using EKrumynas.DTOs.User;
 using EKrumynas.Models;
 
-
-/*
- * Required] public string FirstName { get; set; }
-        [Required] public string LastName { get; set; }
-        [Required] public string Username { get; set; }
-        [Required] public string Email { get; set; }
-        [Required] public byte[] PasswordHash { get; set; }
-        [Required] public byte[] PasswordSalt { get; set; }
-        public string ProfileImage { get; set; }
-        public DateTime CreatedAt { get; }
-
-        public string Country { get; set; }
-        public string Street { get; set; }
-        public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
-
-        public Role Role { get; set; } = Role.USER;
- */
 namespace EKrumynas
 {
     public class AutoMapperProfile : Profile

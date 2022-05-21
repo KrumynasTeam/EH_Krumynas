7bbe8add95526237a8ee914b40dc9ab386db8b1a
Evaldas Grublys <evaldasg@wix.com>
2022-05-21 10:31:48 +0300

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { UserProvider } from './components/contexts/UserContext';
-import { FixedContactMessage } from './components/Messenger/FixedContactMessage';
+import { FixedContactMessage } from './components/messenger/FixedContactMessage';
 import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById("root") as HTMLDivElement);

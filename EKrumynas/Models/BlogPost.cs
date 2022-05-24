391e3a72a4a34a8923a177d34a5f20883d9e57da
Evaldas Grublys <evaldasg@wix.com>
2022-05-24 23:51:03 +0300

    Fix

--- EKrumynas/Models/BlogPost.cs
+++ EKrumynas/Models/BlogPost.cs
@@ -10,9 +10,6 @@ namespace EKrumynas.Models
         [Required] public string Title { get; set; }
         [Required] public string Content { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-<<<<<<< HEAD
-=======
         public string ImageUrl { get; set; }
->>>>>>> main
     }
 }
